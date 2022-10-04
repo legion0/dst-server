326614ccd492dc84633c5dced1c3a65f4c480b00
legion <legion@example.com>
2022-10-04 12:50:22 -0700

    wip

--- dst_mod_add.sh
+++
@@ -1,6 +0,0 @@
-
-CLUSTER_ID="XXX_CLUSTER_NAME_XXX"
-SETTINGS_DIR=/home/dst/.klei/DoNotStarveTogether
-
-"${SETTINGS_DIR}/${CLUSTER_ID}/Master/modoverrides.lua"
-"${SETTINGS_DIR}/${CLUSTER_ID}/Caves/modoverrides.lua"
